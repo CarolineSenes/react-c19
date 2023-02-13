04fdc7072d0c2e365ecdd4ee5b0fe71209a1594e
Caroline SENES <caroline.senes@capgemini.com>
2023-02-13 20:41:10 +0100

    add create user method

---
+++ client/src/apis/users.js
@@ -0,0 +1,17 @@
+const API_USERS = "/api/users";
+
+export async function createUser(newUser) {
+  const response = await fetch("API_USERS", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newUser),
+  });
+  const body = await response.json();
+  if (response.ok) {
+    return body;
+  } else {
+    throw new Error("Error api createUser");
+  }
+}
