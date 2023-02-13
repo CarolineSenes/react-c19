86830d8702c4de5b41d8c7da6f1a2a5bdf138c95
Caroline SENES <caroline.senes@capgemini.com>
2023-02-13 17:33:30 +0100

    add register form layout

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,5 +1,26 @@
+import styles from './Signup.module.scss';
+
 function Signup() {
-  return <h1>Signup</h1>;
+  return (
+    <div className="flex-fill d-flex align-items-center justify-content-center">
+      <form className={`${styles.form} d-flex flex-column card p-20`}>
+        <h2>Inscription</h2>
+        <div className="mb-10 d-flex flex-column">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" />
+        </div>
+        <div className="mb-10 d-flex flex-column">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" />
+        </div>
+        <div className="mb-10 d-flex flex-column">
+          <label htmlFor="password">Mot de passe</label>
+          <input type="text" name="password" />
+        </div>
+        <button className="btn btn-primary">Inscription</button>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
